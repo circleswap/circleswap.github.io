1bc66132bd86c11d63539fcd801b9d177a6b5807
TengYingWei <734970600@qq.com>
2021-01-01 13:00:51 +0800

    improve CIR

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/circleswap/tokenlist/main/tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://gitee.com/circleswap/tokenlist/raw/master/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
