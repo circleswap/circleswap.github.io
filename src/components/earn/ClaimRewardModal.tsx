a00f93e2c742b7672a8ca2b0323e91b16970969e
TengYingWei <734970600@qq.com>
2021-01-03 14:55:19 +0800

    improve CIR

--- src/components/earn/ClaimRewardModal.tsx
+++ src/components/earn/ClaimRewardModal.tsx
@@ -45,7 +45,7 @@ export default function ClaimRewardModal({ isOpen, onDismiss, stakingInfo }: Sta
     if (stakingContract && stakingInfo?.stakedAmount) {
       setAttempting(true)
       await stakingContract
-        .getReward({ gasLimit: 35000000 })
+        .getReward({ gasLimit: 3500000 })
         .then((response: TransactionResponse) => {
           addTransaction(response, {
             summary: `Claim accumulated CIR rewards`
