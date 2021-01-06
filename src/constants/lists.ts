b25e537277745f440c4398e98b756ef80e49210d
TengYingWei <734970600@qq.com>
2021-01-06 12:27:11 +0800

    improve

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://image.zixuan2018.cn/tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://circle.zixuan2018.cn/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
